a6d9d836f5d51fd76c1cbc66e6cb46d72754237e
Godbobo <980742324@qq.com>
2019-10-12 19:58:50 +0800

    隐藏配置文件敏感信息

--- vue/admin/src/config/index.js
+++ vue/admin/src/config/index.js
@@ -1,12 +1,9 @@
 export default {
-  /**
-   * @description 配置显示在浏览器标签的title
-   */
+  // 配置显示在浏览器标签的title
   title: '爱情终是残念',
-  client: 'cGFzc3dvcmQ6MTIz',
-  /**
-   * @description refresh_token在localstorage中存储的时间，默认7天
-   */
+  // oauth认证所需的客户端信息
+  client: 'input your client id',
+  // @description refresh_token在localstorage中存储的时间，默认7天
   refreshTokenExpires: 604800,
   /**
    * @description 是否使用国际化，默认为false
@@ -14,21 +11,15 @@ export default {
    *              用来在菜单中显示文字
    */
   useI18n: false,
-  /**
-   * @description api请求基础路径
-   */
+  // api请求基础路径
   baseUrl: {
     // dev: 'http://web.frp.aqzscn.com/api/',
     dev: 'http://127.0.0.1:521/api/',
     pro: 'http://api.aqzscn.com/'
   },
-  /**
-   * @description 默认打开的首页的路由name值，默认为home
-   */
+  // 默认打开的首页的路由name值，默认为home
   homeName: 'home',
-  /**
-   * @description 需要加载的插件
-   */
+  // 需要加载的插件
   plugin: {
     'error-store': {
       showInHeader: true, // 设为false后不会在顶部显示错误日志徽标
