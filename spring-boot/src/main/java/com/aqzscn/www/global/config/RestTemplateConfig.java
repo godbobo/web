25dc9c3fcc5cbafcabf7a4f97c3ad039dfe7490e
Godbobo <980742324@qq.com>
2019-10-31 17:35:09 +0800

    解决使用restTemplate发送请求时中文乱码的问题

--- spring-boot/src/main/java/com/aqzscn/www/global/config/RestTemplateConfig.java
+++ spring-boot/src/main/java/com/aqzscn/www/global/config/RestTemplateConfig.java
@@ -4,14 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+import java.util.List;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
-        return new RestTemplate(factory);
+        RestTemplate restTemplate = new RestTemplate(factory);
+        // 设置消息转换器的编码格式，防止发出的请求中文乱码
+        List<HttpMessageConverter<?>> list = restTemplate.getMessageConverters();
+        for(HttpMessageConverter<?> httpMessageConverter : list) {
+            if (httpMessageConverter instanceof StringHttpMessageConverter) {
+                ((StringHttpMessageConverter)  httpMessageConverter).setDefaultCharset(Charset.forName("utf-8"));
+            }
+        }
+        return restTemplate;
     }
 
     @Bean
